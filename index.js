d0c4a24aedd3b65bb2271f9f87b10e20a840c013
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-27 22:19:52 +0800

    try-v13-1--use done __1-layer-module

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+  module.exports = getGithubData
+ 
+function getGithubData (done) {
+		    var data1= [
+		    {
+			"repo": "nicco88/personal-portfolio",
+			"url": "https://api.github.com/repos/nicco88/personal-portfolio",
+			"username": "ninabreznik",
+			"time": "2018-04-01T17:50:29Z"
+		    },
+		    {
+			"repo": "freeCodeCamp/devdocs",
+			"url": "https://api.github.com/repos/freeCodeCamp/devdocs",
+			"username": "serapath",
+			"time": "2018-03-27T04:51:07Z"
+		    }]
+		    done(data1)
+	  }
